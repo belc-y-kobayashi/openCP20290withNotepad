a857a85ad9471cf0b09bf66d0047d99d206f0a27
y-kobayashi <y-kobayashi@belc.co.jp>
2023-04-04 13:13:20 +0900

    initial commit

---
+++ cp20290_convert/Converter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cp20290_convert
+{
+    internal class Converter
+    {
+        internal static string ReadSourceFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                var encoding = Encoding.GetEncoding("IBM290");
+                using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                using var reader = new StreamReader(stream, encoding);
+                return reader.ReadToEnd();
+            }
+            else throw new Exception("File Not Found");
+        }
+        internal static string[] SplitAt(string target, int num)
+        {
+            return Regex.Split(target, $"(.{{{num}}})").Where(s => s.Length == num).ToArray();
+        }
+    }
+}
---
+++ cp20290_convert/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace cp20290_convert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            var text = Converter.ReadSourceFile(args[0]);
+            var splitted = Converter.SplitAt(text, 256);
+
+            var tmpFile = Path.GetTempFileName();
+            using (var sw = new StreamWriter(tmpFile))
+            {
+                sw.WriteLine(string.Join("\n", splitted));
+            }
+            System.Diagnostics.Process.Start("notepad.exe", tmpFile);
+        }
+    }
+}
